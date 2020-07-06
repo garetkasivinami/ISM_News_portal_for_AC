4f8acb0b1bf900a2845171183c6ecf425a8401da
Nazariy <n.leut@ukr.net>
2020-07-06 14:58:19 +0300

    Fixed Comments, News and Create News Views

--- ISMNewsPortal/Scripts/imageUpdater.js
+++ ISMNewsPortal/Scripts/imageUpdater.js
@@ -2,7 +2,6 @@
 let imageElement = document.querySelector("#imageElement");
 
 function UpdateImage() {
-    alert("suka blyat");
     imageElement.src = window.URL.createObjectURL(imagePathInput.files[0]);
 }
 
