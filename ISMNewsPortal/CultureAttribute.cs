e76c2c96ecf92a77d2319bf2053ac5fa4e18ce61
Nazariy <n.leut@ukr.net>
2020-07-21 22:59:50 +0300

    Added ua and ru localizations. Fixed some buttons styles.

--- ISMNewsPortal/CultureAttribute.cs
+++ ISMNewsPortal/CultureAttribute.cs
@@ -15,7 +15,7 @@ namespace ISMNewsPortal
             string cultureName = filterContext.RouteData.Values["lang"] as string;
 
             // Список культур
-            List<string> cultures = new List<string>() { "ru", "en" };
+            List<string> cultures = new List<string>() { "ua", "en", "ru" };
             if (!cultures.Contains(cultureName))
             {
                 cultureName = "en";
